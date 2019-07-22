c710b0ca971f8a08cd2789e12db96a14d64fc460
Pranav K <prkrishn@hotmail.com>
2019-07-22 13:45:47 -0700

    Add a custom step

---
+++ src/CustomSteps/TestCustomStep.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Linker;
+using Mono.Linker.Steps;
+using System;
+
+namespace CustomSteps
+{
+    public class TestCustomStep : IStep
+    {
+        public void Process(LinkContext context)
+        {
+            Console.WriteLine("It's me, custom step!");
+        }
+    }
+}
