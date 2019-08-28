6b3ee9475900ec0c4a95f442f882eae9bafe668e
Sven Boemer <sven@svbomer-fedora.guest.corp.microsoft.com>
2019-08-28 09:09:21 -0700

    Make run-it.ps1 work x-plat

--- run-it.ps1
+++ run-it.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 param(
     [string] $app,
     [string] $url,
@@ -10,37 +12,56 @@ param(
 
 dotnet clean
 
-if (Test-Path "obj\")
+if (Test-Path "obj")
 {
     Write-Debug "Deleting obj"
-    Remove-Item "obj\" -r -for
-}    
+    Remove-Item "obj" -r -for
+}
 
-$path = "bin\Release\netcoreapp3.0\win10-x64\publish\"
+if ($IsWindows)
+{
+    $rid = "win10-x64"
+}
+elseif ($IsMacOS)
+{
+    $rid = "osx-x64"
+}
+elseif ($IsLinux)
+{
+    $rid = "linux-x64"
+}
+$path = Join-Path "bin" "Release" "netcoreapp3.0" "$rid" "publish"
 if (Test-Path $path)
 {
     Write-Debug "Deleting $path"
     Remove-Item $path -r -for
 }
 
-dotnet publish -c Release -r win10-x64 /p:PublishTrimmed=$trim /p:LinkAggressively=$aggro /p:LinkAwayReadyToRun=$trimr2r /p:PublishReadyToRun=$r2r /p:PublishSingleFile=$singleFile /bl
+dotnet publish -c Release -r $rid /p:PublishTrimmed=$trim /p:LinkAggressively=$aggro /p:LinkAwayReadyToRun=$trimr2r /p:PublishReadyToRun=$r2r /p:PublishSingleFile=$singleFile /bl
 
 Push-Location $path
 Write-Host ("Size is {0:N2} MB" -f ((Get-ChildItem . -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB))
 
-$app_path = '.\' + $app
+$app_path = Join-Path (Get-Location) $app
+if (-not (Test-Path $app_path))
+{
+    Write-Error "app not found at $app_path"
+    exit
+}
 $app_args = @()
 $console = $url -eq $null -or $url -eq ""
+
+$iterations = 10
 
 if ($time)
 {
     $app_args += ("--time")
 
     $sum = 0
-    for ($i = 0; $i -lt 11; $i++)
+    for ($i = 0; $i -lt ($iterations + 1); $i++)
     {
-        Write-Debug "Starting $app"     
-        $result = Measure-Command {    
+        Write-Debug "Starting $app"
+        $result = Measure-Command {
             $proc = Start-Process -FilePath $app_path -ArgumentList $app_args -PassThru -NoNewWindow -RedirectStandardOutput '.\NUL'
             $proc.WaitForExit()
         }
