ac6881a9ac12df2483acac0a8bc84705787830f4
Sven Boemer <sven@svbomer-fedora.guest.corp.microsoft.com>
2019-08-29 08:50:06 -0700

    Enable R2R for perf collection

--- run.ps1
+++ run.ps1
@@ -8,5 +8,5 @@ $url = "http://localhost:5000/WeatherForecast"
 $trace_name = Join-Path "$PSScriptRoot" "$app"
 
 Push-Location (Join-Path "$PSScriptRoot" "src" "$app")
-& (Join-Path "$PSScriptRoot" "run-it.ps1") -appname "$app" -url "$url" -trimr2r -aggro -trim -time -trace "$trace_name"
+& (Join-Path "$PSScriptRoot" "run-it.ps1") -appname "$app" -url "$url" -r2r -aggro -trim -time -trace "$trace_name"
 Pop-Location
