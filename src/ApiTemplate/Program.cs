7b81a62638aa1555a775b50fd56788726c42cfcd
Ryan Nowak <rynowak@microsoft.com>
2019-09-10 09:37:54 -0700

    Use http only

--- src/ApiTemplate/Program.cs
+++ src/ApiTemplate/Program.cs
@@ -33,6 +33,7 @@ namespace ApiTemplate
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://localhost:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
