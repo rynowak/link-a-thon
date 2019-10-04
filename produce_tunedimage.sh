efdccb8a2062d756c885d79b0f2b537a3f88e48c
Simon Nattress <nattress@gmail.com>
2019-10-04 14:50:06 -0700

    Move tibcdata under coreclrbin
    Move tibcdata to the folder we zip up for server runs

--- produce_tunedimage.sh
+++ produce_tunedimage.sh
@@ -3,8 +3,8 @@
 scriptroot="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-rm -r -f $scriptroot/src/ApiTemplate/tibcdata
-mkdir -p $scriptroot/src/ApiTemplate/tibcdata
-cp $scriptroot/tibcdata/* $scriptroot/src/ApiTemplate/tibcdata
+rm -r -f $scriptroot/src/coreclrbin/tibcdata
+mkdir -p $scriptroot/src/coreclrbin/tibcdata
+cp $scriptroot/tibcdata/* $scriptroot/src/coreclrbin/tibcdata
 
-dotnet publish -c Release src/ApiTemplate/ApiTemplate.csproj -o publish -p:SelfContained=true -p:UseStaticHost=true -p:UsePublishFilterList=true -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:UseTibcData=true -p:UseCrossgen2=true $1
+dotnet publish -c Release src/ApiTemplate/ApiTemplate.csproj -o publish -p:LinkAggressively=true -p:SelfContained=true -p:UseStaticHost=true -p:UsePublishFilterList=true -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:UseTibcData=true -p:UseCrossgen2=true $1
